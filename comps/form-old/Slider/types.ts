7f6f5106c73f78e5fa15bc6658702bcaff7a892c
rizrmd <rizky@avolut.com>
2024-05-19 00:01:49 -0700

    remove form old

--- comps/form-old/Slider/types.ts
+++
@@ -1,5 +0,0 @@
-export type SliderOptions = {
-  step: number;
-  min: { value: number; label: string };
-  max: { value: number; label: string };
-};
--- comps/form-old/type.ts
+++
@@ -1,7 +0,0 @@
-export type FieldOptions = (opt: {
-  data: any;
-  current_name: string;
-  where?: { values: string[] }
-}) => Promise<(string | FieldListItem)[]>
-
-export type FieldListItem = { value: string; label: string }
