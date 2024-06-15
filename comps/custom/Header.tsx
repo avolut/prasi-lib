bf7ed71c06ff50c9dabb526072568f2cfcf40a13
Rizky <rizky@avolut.com>
2024-06-15 10:48:07 +0700

    fix

--- comps/custom/Header.tsx
+++ comps/custom/Header.tsx
@@ -148,10 +148,9 @@ export const Header: FC<{
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-pencil-line"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <path d="M12 20h9" />
                 <path d="M16.5 3.5a2.12 2.12 0 0 1 3 3L7 19l-4 1 1-4Z" />
